7b9d33787b04264d4ca5da22ae0411e6d75206cc
Jere <jeremontoya155@gmail.com>
2022-01-19 18:55:52 -0300

    Primer Archivo Git

---
+++ src/alumnos/Alumnos.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alumnos;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Alumnos {
+    
+    public String nombre;
+    public double nota1;
+    public double nota2;
+    public double nota3;
+    public double nota4;
+    public double nota5;
+
+    public Alumnos(String nombre, double nota1, double nota2, double nota3, double nota4, double nota5) {
+        this.nombre = nombre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+        this.nota5 = nota5;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+
+    public double getNota5() {
+        return nota5;
+    }
+
+    public void setNota5(double nota5) {
+        this.nota5 = nota5;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
